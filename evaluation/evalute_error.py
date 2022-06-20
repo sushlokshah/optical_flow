8ba370f095f4d04c79c05eded8b10c1dab41f809
sushlokshah <sushlokshahkop3@gmail.com>
2022-06-20 14:44:22 +0200

    added code for evaluating model on vkitti dataset

--- evaluation/evalute_error.py
+++ evaluation/evalute_error.py
@@ -30,7 +30,7 @@ def evalute_kitti_error(path):
             gt_flow = cv.imread(gt_path + "/" + i, cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
             # print(gt_flow)
             result_flow = cv.imread(path + "/" + i, cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
-            error_map, AAE, EPE, absolute_error = error(gt_flow,result_flow)
+            error_map, AAE, EPE, absolute_error = error_kitti(gt_flow,result_flow)
             if not os.path.exists(path + "/error_vis"):
                 os.makedirs(path + "/error_vis")
             cv.imwrite(path + "/error_vis/" + i ,error_map)
@@ -46,7 +46,7 @@ def evalute_kitti_error(path):
     df.to_csv(path + "/errors_flow_noc" + "/error_data2.csv")
     print(df.describe())
     
-def error(gt_flow,result_flow):
+def error_kitti(gt_flow,result_flow):
     """calculate error maps and AAE,EPE,absolute_error
 
     Args:
@@ -83,101 +83,88 @@ def error(gt_flow,result_flow):
     error_map = cv.cvtColor(hsv, cv.COLOR_HSV2RGB)
     return error_map, AAE, EPE, absolute_error
 
+def error_vkitti(gt_flow,result_flow):
+    w,h,_ = gt_flow.shape
+    mask = gt_flow[:,:,0]*(1/(2**16 - 1))
+    count = np.count_nonzero(mask) 
+    
+    actual_gt = gt_flow[:,:,1:]*(1/(2**16 - 1))
+    actual_gt = actual_gt*2 - 1
+    actual_gt[:,:,0] = actual_gt[:,:,0]*(h-1)
+    actual_gt[:,:,1] = actual_gt[:,:,1]*(w-1)
+    
+    actual_result = result_flow[:,:,1:]*(1/(2**16 - 1))
+    actual_result = actual_result*2 - 1
+    actual_result[:,:,0] = actual_result[:,:,0]*(h-1)
+    actual_result[:,:,1] = actual_result[:,:,1]*(w-1)
+    
+    error_map = (actual_gt - actual_result)
+    error_map[:,:,0] = error_map[:,:,0]*mask
+    error_map[:,:,1] = error_map[:,:,1]*mask
+    num = actual_gt[:,:,0]*actual_result[:,:,0] + actual_gt[:,:,1]*actual_result[:,:,1] + 1
+    num = num*mask
+    den = ((np.square(actual_gt[:,:,0])+ np.square(actual_gt[:,:,1])+ 1)*(np.square(actual_result[:,:,0])+ np.square(actual_result[:,:,1])+ 1))*mask
+    den = np.sqrt(den)
+    AAE = np.sum(np.arccos(num/(den + 0.000000001)))*(1/(count+0.0000000001))
+    EPE = np.sum(np.sqrt(np.square(error_map[:,:,0])+ np.square(error_map[:,:,1])))*(1/count)
+    absolute_error = np.sum(np.abs(error_map[:,:,0]) + np.abs(error_map[:,:,1]))*(1/count)
+    hsv = np.zeros((error_map.shape[0], error_map.shape[1], 3), dtype=np.uint8)
+    error_map_magnitude, error_map_angle = cv.cartToPolar(error_map[..., 0].astype(np.float32), error_map[..., 1].astype(np.float32))
+
+    nans = np.isnan(error_map_magnitude)
+    if np.any(nans):
+        nans = np.where(nans)
+        error_map_magnitude[nans] = 0.
+        
+    hsv[..., 0] = error_map_angle * 180 / np.pi / 2
+    hsv[..., 1] = cv.normalize(error_map_magnitude, None, 0, 255, cv.NORM_MINMAX)
+    hsv[..., 2] = 255
+    error_map = cv.cvtColor(hsv, cv.COLOR_HSV2RGB)
+    return error_map, AAE, EPE, absolute_error    
+
+def evaluate_vkitti_error(seq):
+    path = "/home/sushlok/optical_flow/GMFlowNet/result/0020/vkitti_{}/".format(seq)
+    list_of_flow_maps = sorted(os.listdir(path))
+    gt_path = "/home/sushlok/new_approach/datasets/vkitti/vkitti_1.3.1_flowgt/0020/{}".format(seq)
+    total_AAE = []
+    total_EPE = []
+    total_absolute_error = []
+    for i in list_of_flow_maps:
+        if(i[-4:] == ".png"):
+            gt_flow = cv.imread(gt_path + "/" + i, cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
+            # print(gt_flow)
+            result_flow = cv.imread(path + "/" + i, cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
+            error_map, AAE, EPE, absolute_error = error_kitti(gt_flow,result_flow)
+            if not os.path.exists(path + "/error_vis"):
+                os.makedirs(path + "/error_vis")
+            cv.imwrite(path + "/error_vis/" + i ,error_map)
+            total_AAE.append(AAE)
+            total_EPE.append(EPE)
+            total_absolute_error.append(absolute_error)
+    data_error = {
+        "AAE" : total_AAE,
+        "EPE" : total_EPE,
+        "absolute_error": total_absolute_error
+    }
+    df = pd.DataFrame.from_dict(data_error)
+    df.to_csv(path + "/error_data2.csv")
+    print(df.describe())
+
+
 if __name__ == "__main__":
     Methods_list = [
-    #     "Farneback_flow",
-    # "Farneback_flow_Fast_features",
-    # "lucas_kanade_GoodFeaturesToTrack",
-    # "Farneback_flow_GoodFeaturesToTrack"  ,
-    # "spynet",
-    # "flownet2",
-    # "spynet_Fast_features",
-    # "flownet2_Fast_features",
-    # "spynet_GoodFeaturesToTrack",
-    # "flownet2_GoodFeaturesToTrack",
-    # "lucas_kanade_Fast_features",
-    # "liteflownet",
-    # "liteflownet_Fast_features_10",
-    # "liteflownet_Fast_features_30",
-    # "liteflownet_Fast_features_60",
-    # "liteflownet_GoodFeaturesToTrack",
-    # "pwcnet",
-    # "pwcnet_Fast_features_10",
-    # "pwcnet_Fast_features_30",
-    # "pwcnet_Fast_features_60",
-    # "pwcnet_GoodFeaturesToTrack",
-    # "pwcnet_ft",
-    # "pwcnet_ft_Fast_features_10",
-    # "pwcnet_ft_Fast_features_30",
-    # "pwcnet_ft_Fast_features_60",
-    # "pwcnet_ft_GoodFeaturesToTrack",
-    # "VCN",
-    # "VCN_Fast_features_10",
-    # "VCN_Fast_features_30",
-    # "VCN_Fast_features_60",
-    # "VCN_GoodFeaturesToTrack",
-    # "maskflownet_final",
-    # "maskflownet_ft_flying_chair_Fast_features_30",
-    # "maskflownet_final_Fast_features_10",
-    # "maskflownet_ft_flying_chair_Fast_features_60",
-    # "maskflownet_final_Fast_features_30",
-    # "maskflownet_ft_flying_chair_GoodFeaturesToTrack",
-    # "maskflownet_final_Fast_features_60",     
-    # "maskflownet_final_GoodFeaturesToTrack",  
-    # "maskflownet_ft_flying_chair",                      
-    # "maskflownet_ft_flying_chair_Fast_features_10",
-    # "raft_kitti",
-    # "raft_mixed_data_Fast_features_30",
-    # "raft_kitti_Fast_features_10",
-    # "raft_mixed_data_Fast_features_60",
-    # "raft_kitti_Fast_features_30",
-    # "raft_mixed_data_GoodFeaturesToTrack",
-    # "raft_kitti_Fast_features_60",     
-    # "raft_kitti_GoodFeaturesToTrack",  
-    # "raft_mixed_data",                      
-    # "raft_mixed_data_Fast_features_10"
-    "gma_kitti",                           
-    "gma_plus_kitti_Fast_features_10",          
-    "gma_p_only_kitti_Fast_features_30",
-    "gmflownet_kitti_Fast_features_60",
-    "gma_kitti_Fast_features_10",          
-    "gma_plus_kitti_Fast_features_30",          
-    "gma_p_only_kitti_Fast_features_60",          
-    "gmflownet_kitti_GoodFeaturesToTrack",
-    "gma_kitti_Fast_features_30",
-    "gma_plus_kitti_Fast_features_60",         
-    "gma_p_only_kitti_GoodFeaturesToTrack",
-    "gmflownet_mixed",
-    "gma_kitti_Fast_features_60",        
-    "gma_plus_kitti_GoodFeaturesToTrack",
-    "gma_p_only_mixed_data",
-    "gmflownet_mixed_Fast_features_10",
-    "gma_kitti_GoodFeaturesToTrack",       
-    "gma_plus_mixed_data",                      
-    "gma_p_only_mixed_data_Fast_features_10",     
-    "gmflownet_mixed_Fast_features_30",
-    "gma_mixed_data",                      
-    "gma_plus_mixed_data_Fast_features_10",     
-    "gma_p_only_mixed_data_Fast_features_30",
-    "gmflownet_mixed_Fast_features_60",
-    "gma_mixed_data_Fast_features_10",     
-    "gma_plus_mixed_data_Fast_features_30",     
-    "gma_p_only_mixed_data_Fast_features_60",     
-    "gmflownet_mixed_GoodFeaturesToTrack",
-    "gma_mixed_data_Fast_features_30",     
-    "gma_plus_mixed_data_Fast_features_60",     
-    "gma_p_only_mixed_data_GoodFeaturesToTrack",
-    "gma_mixed_data_Fast_features_60",
-    "gma_plus_mixed_data_GoodFeaturesToTrack",
-    "gmflownet_kitti",
-    "gma_mixed_data_GoodFeaturesToTrack",
-    "gma_p_only_kitti",
-    "gmflownet_kitti_Fast_features_10",
-    "gma_plus_kitti",                      
-    "gma_p_only_kitti_Fast_features_10",        
-    "gmflownet_kitti_Fast_features_30"
+        "15-deg-left",
+        "15-deg-right",
+        "30-deg-left",
+        "30-deg-right",
+        "clone",
+        "fog",
+        "morning",
+        "overcast",
+        "rain",
+        "sunset"
     ]
     
     for method in Methods_list:
         print(method)
-        evalute_kitti_error("results/"+ method)
+        evaluate_vkitti_error(method)
--- temp/temp.py
+++ temp/temp.py
@@ -1,14 +1,6 @@
-from mmflow.apis import inference_model, init_model
 import cv2 as cv
 import numpy as np
-config_file = 'mmflow/configs/pwcnet/pwcnet_ft_4x1_300k_sintel_final_384x768.py'
-checkpoint_file = 'mmflow/mim/pwcnet_ft_4x1_300k_sintel_final_384x768.pth'
-device = 'cuda:0'
-# init a model
-model = init_model(config_file, checkpoint_file, device=device)
-# inference the demo image
-image = inference_model(model, 'mmflow/demo/frame_0001.png', 'mmflow/demo/frame_0002.png')
-flow = np.zeros([image.shape[0],image.shape[1],3])
-flow[:,:,0] = 1
-flow[:,:,1:] = image
-cv.imwrite("flow_out.png",flow)
+
+img1 = cv.imread('/home/sushlok/new_approach/datasets/vkitti/vkitti_1.3.1_flowgt/0001/fog/00000.png',cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
+img2 = cv.imread('/home/sushlok/optical_flow/GMFlowNet/result/vkitti/000000.png',cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
+print(img1[:,:,0])
